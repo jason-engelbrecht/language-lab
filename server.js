f4759d4389385e1041c3bfa5c0702ff260accf78
Kaephas <kkain@mail.greenriver.edu>
2020-02-04 19:36:08 -0800

    File upload - run npm i for new dependency

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 import express from 'express';
+import fileUpload from 'express-fileupload';
 const path = require('path');
 const server = express();
 
-server.use(express.static('public'));
+server.use(express.static('public'), fileUpload());
 // server.use(express.static('dist'));
 
 server.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-server.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+server.post('/upload', (req, res) => {
+    if(req.files === null) {
+        return res.status(400).json({ msg: "No file uploaded" });
+    }
 
+    const file = req.files.file;
+
+    file.mv(`${__dirname}/src/client/uploads/${file.name}`, err => {
+        if(err) {
+            console.error(err);
+            return res.status(500).send(err);
+        }
+
+        res.json({ filename: file.name, filePath: `/uploads/${file.name}`});
+    })
+});
+
+server.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
--- src/client/components/pages/sections/FileUploadSection.js
+++ src/client/components/pages/sections/FileUploadSection.js
@@ -1,27 +1,61 @@
-import React, {Component} from "react";
-import {MDBRow} from "mdbreact";
+import React, {Component, Fragment, useState } from "react";
+import axios from "axios";
 
-class FileUploadSection extends Component {
-    render(){
-        return(
-            <MDBRow class="mb-4">
-                <div className="input-group">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text" id="upload-label">Upload</span>
-                    </div>
+import {MDBCol, MDBRow} from "mdbreact";
+
+const FileUploadSection = () => {
+    // react hooks => generates methods?
+    const [file, setFile] = useState(''); // sets default
+    // sets the starting filename to "choose file"
+    const [filename, setFilename] = useState('Choose File');
+    const[uploadedFile, setUploadedFile] = useState({});
+
+    const onChange = e => {
+        // html file input allows multiples so stores as an array
+        setFile(e.target.files[0]);
+        // get the name property of the file
+        setFilename(e.target.files[0].name);
+    };
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        const formData = new FormData();
+        // constant was created on server.js 'file', setting it with the hook setFile
+        formData.append('file', file);
+
+        try {
+            const res = await axios.post('/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            const {fileName, filePath} = res.data;
+            setUploadedFile( {fileName, filePath });
+        } catch(err) {
+            console.log(err);
+        }
+    };
+
+    return(
+        <Fragment>
+            <MDBRow className="mb-4">
+                <form className="input-group mb-3" onSubmit={onSubmit}>
                     <div className="custom-file">
-                        <form action="">
-                            <input type="file" className="file-upload" id="xlsx-upload"/>
-                            <label className="custom-file-label" htmlFor="xlsx-upload">Choose file</label>
-                            <input type="submit" name="Upload"/>
-                        </form>
-
+                        {/*runs the 'onchange' function onchange*/}
+                        <input type="file" className="custom-file-input" id="inputGroupFile" onChange={onChange}/>
+                        {/* sets filename to the state of 'filename' variable */}
+                        <label className="custom-file-label" htmlFor="inputGroupFile"
+                               aria-describedby="inputGroupFileAddon">{filename}</label>
                     </div>
-                </div>
+                    <div className="input-group-append">
+                        <input type="submit" value="Upload"
+                               className="input-group-text btn-success" id="inputGroupFileAddon" />
+                    </div>
+                </form>
             </MDBRow>
-        )
-    }
-}
+        </Fragment>
+    )
+
+};
 
 export default FileUploadSection;
-
