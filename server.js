c04a49efce154178d5f20c2159937901108b5b00
Jason Engelbrecht <47205928+jason-engelbrecht@users.noreply.github.com>
2020-02-11 09:10:25 -0800

    Prepare excel data for mongo(excel->json)

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ server.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+import excelToJson from 'convert-excel-to-json';
+
 server.post('/upload', (req, res) => {
     if(req.files === null) {
         return res.status(400).json({ msg: "No file uploaded" });
@@ -17,11 +19,25 @@ server.post('/upload', (req, res) => {
 
     const file = req.files.file;
 
-    file.mv(`${__dirname}/src/client/uploads/${file.name}`, err => {
+    file.mv(`${__dirname}/uploads/${file.name}`, err => {
         if(err) {
             console.error(err);
             return res.status(500).send(err);
         }
+
+        //convert excel upload to json object
+        const result = excelToJson({
+            sourceFile: `${__dirname}/uploads/${file.name}`,
+            //1st row is header - don't include
+            header: {
+                rows: 1
+            },
+            //column header as keys
+            columnToKey: {
+                '*': '{{columnHeader}}'
+            }
+        });
+        console.log(result);
 
         res.json({ filename: file.name, filePath: `/uploads/${file.name}`});
     })
