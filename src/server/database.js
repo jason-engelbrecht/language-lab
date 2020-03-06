8440e39d290c820444714f6c5279bf4b5125c399
Kaephas Kain <kkain@mail.greenriver.edu>
2020-03-06 10:47:23 -0800

    dev branch local db

--- src/server/database.js
+++ src/server/database.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 //connect to mongo w mongoose
 
-// mongoose.connect('mongodb://localhost:27017/LLTest', {useNewUrlParser: true});
-mongoose.connect('mongodb://Boolean:Hooligans1@ds023442.mlab.com:23442/heroku_8tbqhjvr', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/LLTest', {useNewUrlParser: true});
+// mongoose.connect('mongodb://Boolean:Hooligans1@ds023442.mlab.com:23442/heroku_8tbqhjvr', {useNewUrlParser: true});
 
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
