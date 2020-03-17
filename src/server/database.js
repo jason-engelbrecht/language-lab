31939ffe114cc89a6e7e046de15861c602ab57f6
Jason Engelbrecht <47205928+jason-engelbrecht@users.noreply.github.com>
2020-03-17 10:43:29 -0700

    Change db connection

--- src/server/database.js
+++ src/server/database.js
@@ -3,8 +3,8 @@ import bcrypt from 'bcrypt';
 const saltRounds = 10;
 
 //connect to mongo w mongoose
-mongoose.connect('mongodb://localhost:27017/LLTest', {useNewUrlParser: true});
-// mongoose.connect('mongodb://Boolean:Hooligans1@ds023442.mlab.com:23442/heroku_8tbqhjvr', {useNewUrlParser: true});
+// mongoose.connect('mongodb://localhost:27017/LLTest', {useNewUrlParser: true});
+mongoose.connect('mongodb://Boolean:Hooligans1@ds023442.mlab.com:23442/heroku_8tbqhjvr', {useNewUrlParser: true});
 
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
